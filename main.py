aba0d07c2e37917ae346c6b51602904f79a44993
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-08-01 03:46:04 +0700

    Add Arta in Player Movement

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ game.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 1920, 1080
 PLAYER_SPEED = 1
 
-# Initialize screen
+# Screen
 screen = game.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game.display.set_caption("Elven Abyss")
 icon_image = game.image.load("Assets/Sprite/Sebastian_Icon.png")
@@ -18,11 +18,8 @@ player_img = game.image.load("Assets/Sprite/Player_Anabelle.png")
 player_pos = [(SCREEN_WIDTH // 2) - (player_img.get_width()), (SCREEN_HEIGHT // 2) - (player_img.get_height())] # x, y
 movement = [0, 0]
 
-# Enemy
-enemy_img = game.image.load("Assets/Sprite/36px-Red_Sludge.png")
-enemy_pos = (200 - enemy_img.get_width(), (SCREEN_HEIGHT // 2) - (enemy_img.get_height()))
-
 def handle_events():
+    """similat to void Update() in unity"""
     global movement
     global player_img
     for event in game.event.get():
@@ -37,8 +34,10 @@ def handle_events():
                 player_img = game.image.load("Assets/Sprite/Player_Anabelle.png")
             elif event.key == game.K_a:
                 movement[0] = -PLAYER_SPEED
+                player_img = game.image.load("Assets/Sprite/Player_Anabelle_Left.png")
             elif event.key == game.K_d:
                 movement[0] = PLAYER_SPEED
+                player_img = game.image.load("Assets/Sprite/Player_Anabelle_Right.png")
         elif event.type == game.KEYUP:
             if event.key in (game.K_a, game.K_d):
                 movement[0] = 0
@@ -47,12 +46,13 @@ def handle_events():
     return True
 
 def update_player_position():
+    """update player position everytime"""
     player_pos[0] = max(0, min(player_pos[0] + movement[0], SCREEN_WIDTH - player_img.get_width()))
     player_pos[1] = max(0, min(player_pos[1] + movement[1], SCREEN_HEIGHT - player_img.get_height()))
 
 def draw(screen_color):
+    """draw everything"""
     screen.fill(screen_color)
-    screen.blit(enemy_img, enemy_pos)
     screen.blit(player_img, player_pos)
     game.display.update()
 
@@ -63,11 +63,7 @@ def main():
     while run:
         run = handle_events()
         update_player_position()
-        if abs(player_pos[0] - enemy_pos[0]) < 50 and abs(player_pos[1] - enemy_pos[1]) < 50:
-            screen_color = (150, 50 ,50)
-        else:
-            screen_color = (80, 60 ,100)
-        draw(screen_color)
+        draw((0, 0, 0))
         clock.tick(120)
     game.quit()
 
