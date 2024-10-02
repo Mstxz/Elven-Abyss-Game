bfd5cae62713590cc7ec34510154311ea1771d2f
Hundredz <artasitara@gmail.com>
2024-10-03 01:02:19 +0700

    feat: add Player Movement, Enemy Movement and Camera Movement

---
+++ Godot_Main/script/Camera2D.py
@@ -0,0 +1,29 @@
+from godot import exposed, export
+from godot import *
+
+
+@exposed
+class Camera2D(Camera2D):
+
+	player = None
+
+	def _ready(self):
+		"""
+		Called every time the node is added to the scene.
+		Initialization here.
+		"""
+		self.player = self.get_node("/root/Node2D/Player")
+		
+		pass
+		
+	def _process(self, delta):
+		
+		self.lock_camera(delta)
+		
+	def lock_camera(self, delta):
+		if self.player:
+			# Get the player's position
+			player_position = self.player.position
+
+			# Lock the camera to the player's position
+			self.position = player_position
---
+++ Godot_Main/script/Player.py
@@ -0,0 +1,54 @@
+from godot import exposed, export
+from godot import *
+
+
+@exposed
+class Player(KinematicBody2D):
+
+	# member variables here, example:
+	speed = export(float, default=150.0)
+	velocity = Vector2()
+
+	def _ready(self):
+		"""
+		Called when the node is added to the scene.
+		Initialization happens here.
+		"""
+		self.sprite = self.get_node("AnimatedSprite")
+		pass
+
+	def _process(self, delta):
+		"""
+		Called every frame. Use it for real-time input handling.
+		"""
+		self.move(delta)
+
+	def move(self, delta):
+		# Get input axis for horizontal movement
+		direction_x = Input.get_axis("left", "right")
+		direction_y = Input.get_axis("up", "down")
+		
+		if direction_x or direction_y:
+			self.velocity.x = direction_x * self.speed
+			self.velocity.y = direction_y * self.speed
+			#if direction_x or (direction_x and direction_y):
+				#self.sprite.play("run_l")
+
+			if direction_x < 0:
+				self.sprite.flip_h = False
+
+			elif direction_x > 0:
+				self.sprite.flip_h = True
+			
+			#elif direction_y > 0:
+				#self.sprite.play("run_d")
+			#elif direction_y < 0:
+				#self.sprite.play("run_u")
+				
+		else:
+			self.velocity.x = 0
+			self.velocity.y = 0  # Stop moving when there's no input
+			self.sprite.flip_h = False
+			#self.sprite.play("default")
+		# Move the KinematicBody2D
+		self.velocity = self.move_and_slide(self.velocity)
