e8db9c238c1cc87354e2d671b026bf843a175db6
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-10-13 02:24:43 +0700

    commit???

---
+++ Godot_Main/script/Enemy/Melee_Enemy.py
@@ -0,0 +1,42 @@
+from godot import exposed, export
+from godot import *
+
+
+@exposed
+class Melee_Enemy(KinematicBody2D):
+
+	# member variables here, example:
+	speed = export(float, default=100.00)
+	atk = export(float, default=10.00)
+	hp = export(float, default=100.0)
+	defense = export(float, default=0.0)
+	player = None
+	player_see = export(bool, default = False) 
+
+	def _process(self, delta):
+		if self.player_see:
+			direction = self.player.position - self.position
+			
+			# Normalize direction
+			direction = direction.normalized()
+			
+			# Set velocity based on direction and speed
+			self.velocity = direction * self.speed
+			
+			# Move the enemy using move_and_slide for proper physics handling
+			self.move_and_slide(self.velocity)
+	
+	def _on_Area2D_body_entered(self, body):
+		print("end")
+		self.player = body
+		self.player_see = True
+		
+	def _on_Area2D_body_exited(self, body):
+		self.player = None
+		self.player_see = False
+		
+	def take_damage(self, dmg):
+		self.hp -= dmg
+		
+	def heal(self, amount):
+		self.hp += amount
---
+++ Godot_Main/script/Player/Player.py
@@ -0,0 +1,49 @@
+"""Player System"""
+from godot import exposed, export
+from godot import *
+
+@exposed
+class Player(KinematicBody2D):
+
+	# member variables here, example:
+	speed = export(float, default=150.0)
+	atk = export(float, default=10.00)
+	hp = export(float, default=100.0)
+	exp = export(int, default=0)
+	mana = export(float, default=100.0)
+	defense = export(float, default=0.0)
+	critrate = export(float, default=0.0)
+	critdmg = export(float, default=50.0)
+
+	def _ready(self):
+		"""
+		Called when the node is added to the scene.
+		Initialization happens here.
+		"""
+		self.sprite = self.get_node("AnimatedSprite")
+		pass
+
+	def _process(self, delta):
+		"""
+		Called every frame. Use it for real-time input handling.
+		"""
+		self.move(delta)
+
+	def move(self, delta):
+		"""Player Movement"""
+		direction_x = Input.get_axis("ui_left", "ui_right")
+		direction_y = Input.get_axis("ui_up", "ui_down")
+		velocity = Vector2(direction_x, direction_y)
+
+		if velocity.length() > 0:
+			velocity = velocity.normalized()
+
+		self.move_and_slide(velocity * self.speed)
+
+	def take_damage(self, dmg):
+		"""Player can take damage"""
+		self.hp -= dmg
+
+	def heal(self, amount):
+		"""Heal Player"""
+		self.hp += amount
