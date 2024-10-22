9608dfdbda25a988c1e72b99a1d39393a0cd9827
Hundredz <artasitara@gmail.com>
2024-10-23 00:43:46 +0700

    Feat(add): Range_Enemy Attack, Range_Enemy Attack Delay, Arrow scene, Arrow Script

--- Godot_Main/script/Range_Enemy.py
+++ Godot_Main/script/Range_Enemy.py
@@ -1,6 +1,7 @@
 from godot import exposed, export
 from godot import *
 
+projectile = ResourceLoader.load("res://scene/Arrow.tscn")
 
 @exposed
 class Range_Enemy(KinematicBody2D):
@@ -12,6 +13,7 @@ class Range_Enemy(KinematicBody2D):
 	hp = export(float, default=100.0)
 	defense = export(float, default=0.0)
 	player = None #use to store player objects
+	acting = export(bool, default=False)
 	knockbacked = Vector2() #set in take_damage and reduce by a rate in each _progress
 	velocity = Vector2()
 	minrange = 130 #minimum distance until enemy runs away from player
@@ -22,10 +24,12 @@ class Range_Enemy(KinematicBody2D):
 		#prepares require nodes
 		self.sprite = self.get_node("AnimatedSprite") #enemy sprite
 		self.healthbar = self.get_node("Viewport/HealthBar") #enemy healthbar
+		self.main = self.get_node("/root/Node2D")
 	
 	def _process(self, delta):
 		'''runs every frame'''
 		self.movement() #check movement every frames
+		self.shoot()
 		
 	def movement(self):
 		'''handle all kind of enemy movement'''
@@ -70,7 +74,57 @@ class Range_Enemy(KinematicBody2D):
 			
 			# Move the enemy using move_and_slide for proper physics handling
 			self.move_and_slide(self.velocity)
+	
+	def wait(self,time,funcname,para=Array()):
+		'''see example in shoot()'''
+		timer = Timer.new()
+		timer.one_shot = True
+		self.add_child(timer)
+		
+		# Connect the timeout signal to a custom method
+		timer.connect("timeout", self, funcname, Array(para))
+		timer.connect("timeout", self, 'cleartimer', Array([timer]))
+		# Start the timer
+		timer.start(time)
+	
+	def cleartimer(self,timer):
+		'''sole purpose to delete timer made from wait()'''
+		timer.queue_free()
+	
+	def cooldown(self):
+		'''frequently use to let the player act after the timer'''
+		self.acting = False
+	
+	def shoot(self,part=0):
+		'''enemy shoots projectile toward player'''
+		if not part:
+			if not self.acting:
+				#acting is as it name suggest to prevent spam and
+				#keep the animation running
+				self.acting = True
+				#self.sprite.play()
+				if self.player.position.x > self.position.x:
+					self.sprite.flip_h = True
+				else:
+					self.sprite.flip_h = False
+				self.wait(0.3,'shoot',[part+1])
+		elif part == 1:
+			# 'projectile' is loaded scene sees at the start of this script
+			bullet = projectile.instance()
+			#get direction from mousepos turn it into proper angle value
+			direction = (self.position - self.player.position).angle()
+			#set projectile property
+			bullet.direction = direction
+			bullet.spawnpos = self.position + (self.position - self.player.position) * -0.22
+			bullet.spawnrot = direction
+			bullet.speed = 50
+			bullet.duration = 6
+			#add it
+			self.main.add_child(bullet)
 			
+			#set self.acting back to False after the set time
+			self.wait(1,'cooldown')
+	
 	def _on_Area2D_body_entered(self, body):
 		'''when player in area2d, enemy will see'''
 		if str(body.name) == "Player": #prevent recognizing other kinematic2d
