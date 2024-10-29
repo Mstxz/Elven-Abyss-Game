7a57f10378fcee8d052e5c3b37d27e792ab1e522
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-10-29 12:22:04 +0700

    Finally, change scene is worked! YAY

--- Godot_Main/script/Stairs_Interaction.py
+++ Godot_Main/script/Stairs_Interaction.py
@@ -18,20 +18,21 @@ class InteractableArea(Area2D):
 		self.connect("body_exited", self, "_on_Area2D_body_exited")
 
 	def _on_Area2D_body_entered(self, body):
+		print("Enter:",str(body.name), body.filename)
 		# Check if the player has entered
-		if body.name == "Player" and body.has_node("CollisionShape2D"):
-			print("Player Entered, Next scene will be:")
+		if str(body.name) == "Player":
+			print("Player Entered")
 			self.player_in_area = True
 
 	def _on_Area2D_body_exited(self, body):
+		print("Exit:",body.name)
 		# Check if the player has exited
-		if body.name == "Player" and body.has_node("CollisionShape2D"):
+		if str(body.name) == "Player":
 			print("Player Exited")
 			self.player_in_area = False
 
-	def _process(self, delta):
-		# Check if the player is in area and 'F' key is pressed
-		if Input.is_action_just_pressed("Interact"):
+	def _process(self, delta):		# Check if the player is in area and 'F' key is pressed
+		if self.player_in_area == True and Input.is_action_just_pressed("Interact"):
 			random_scene_path = random.choice(self.stages)
 			random_scene = ResourceLoader.load(random_scene_path)
 			self.get_tree().change_scene_to(random_scene)
