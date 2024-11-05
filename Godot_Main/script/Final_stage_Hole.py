704e5f0e68e7b3eae79e42449268c06ffac7ee99
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-11-06 00:56:07 +0700

    Fix final Hole lost

---
+++ Godot_Main/script/Final_stage_Hole.py
@@ -0,0 +1,43 @@
+from godot import exposed, export
+from godot.bindings import Area2D, Input, ResourceLoader, Timer
+import random
+
+@exposed
+class InteractableArea(Area2D):
+	# List of available stages
+	stages = [
+		("res://scene/Stage_Final.tscn","Final")
+	]
+	player_in_area = False
+	
+	enemy_count = 0
+	
+	def _ready(self):
+		# Connect signals and create a timer
+		self.connect("body_entered", self, "_on_Area2D_body_entered")
+		self.connect("body_exited", self, "_on_Area2D_body_exited")
+		
+	def _on_Area2D_body_entered(self, body):
+		#print("Enter:",str(body.name), body.filename)
+		# Check if the player has entered
+		if str(body.name) == "Player":
+			#print("Player Entered")
+			self.player_in_area = True
+
+	def _on_Area2D_body_exited(self, body):
+		#print("Exit:",body.name)
+		# Check if the player has exited
+		if str(body.name) == "Player":
+			#print("Player Exited")
+			self.player_in_area = False
+
+	def _process(self, delta):		# Check if the player is in area and 'F' key is pressed
+		if self.player_in_area == True and Input.is_action_just_pressed("Interact"):
+			self.loading = self.get_node("/root").get_child(5).get_node("Loading")
+			self.loading.leave()
+			self.loading.animation.connect("animation_finished", self, "on_animation_finished")
+
+	def on_animation_finished(self, anim_name):
+		random_scene_path = random.choice(self.stages)
+		Scenechange = self.get_tree().get_root().get_node("/root/Scenechange")
+		Scenechange.load_new(str(random_scene_path[0]),str(random_scene_path[1]))
