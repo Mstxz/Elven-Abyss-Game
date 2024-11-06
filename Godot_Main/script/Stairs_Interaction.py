a691036c610293195672a059567fc939a9265efb
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-11-07 03:02:31 +0700

    Update All Scenes + Add new stage

--- Godot_Main/script/Stairs_Interaction.py
+++ Godot_Main/script/Stairs_Interaction.py
@@ -8,7 +8,8 @@ class InteractableArea(Area2D):
 	stages = [
 		("res://scene/Stage_R01.tscn", "Stage1"),
 		("res://scene/Stage_R02.tscn", "Stage2"),
-		("res://scene/Stage_R03.tscn", "Stage3")
+		("res://scene/Stage_R03.tscn", "Stage3"),
+		("res://scene/Stage_R03.tscn", "Stage4") #Good luck na //UwU//
 	]
 	player = None
 	
