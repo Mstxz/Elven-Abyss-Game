8badc3d330fde5b23c7795dcede794ea44293f4b
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-11-06 17:54:19 +0700

    Final Door Bug Fix

--- Godot_Main/script/final_Door.py
+++ Godot_Main/script/final_Door.py
@@ -1,6 +1,5 @@
 from godot import exposed, export
 from godot.bindings import Area2D, Input, ResourceLoader, Timer
-import random
 
 @exposed
 class InteractableArea(Area2D):
@@ -31,7 +30,7 @@ class InteractableArea(Area2D):
 
 	def _process(self, delta):		# Check if the player is in area and 'F' key is pressed
 		if self.player_in_area == True and Input.is_action_just_pressed("Interact"):
-			self.loading = self.get_node("/root").get_child(2).get_node("Loading")
+			self.loading = self.get_node("/root").get_child(5).get_node("Loading")
 			self.loading.leave()
 			self.loading.animation.connect("animation_finished", self, "on_animation_finished")
 
