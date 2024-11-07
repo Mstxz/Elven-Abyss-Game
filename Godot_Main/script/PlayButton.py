1c062c99b14f0dac25fdbcdf3247e0ee386ccea5
Noken <127421730+KuronoKen@users.noreply.github.com>
2024-11-07 18:34:41 +0700

    Decorate Choose Element UI, New Main Menu BG, and Fireball Sprite

--- Godot_Main/script/PlayButton.py
+++ Godot_Main/script/PlayButton.py
@@ -1,15 +1,22 @@
 from godot import exposed, export
 from godot import *
 
+elemdict = {
+	'Water' : Color(0,0.36,1,1),
+	'Fire' : Color(1,0.14,0,1),
+	'Wind' : Color(0,0.76,0.47,1),
+	'Earth' : Color(0.75,0.54,0,1)
+}
 
 @exposed
 class PlayButton(Button):
 	
-	element = export(str, default='Stick')
+	element = export(str, default='Water')
 	
 	def _ready(self):
 		self.connect("pressed", self, "on_button_pressed")
 		self.connect("mouse_entered", self, "on_mouse_entered")
+		self.connect("mouse_exited", self, "on_mouse_exit")
 		self.loading = self.get_node("../../../Loading")
 		self.PlayerVar = self.get_tree().get_root().get_node("/root/PlayerVar")
 		pass
@@ -44,3 +51,23 @@ class PlayButton(Button):
 	
 	def on_mouse_entered(self):
 		self.get_node("HoverSfx").play()
+		self.tweenvalue('modulate',Color(1,1,1,1),elemdict[str(self.element)],0.1)
+	
+	def on_mouse_exit(self):
+		self.tweenvalue('modulate',elemdict[str(self.element)],Color(1,1,1,1),0.1)
+	
+	def tweenvalue(self,prop,start,end,time):
+		#basically automatically change value multiple time to make the illusion of animation
+		tween = self.get_node('Tween')
+		tween.interpolate_property(
+			self,  # Target object
+			prop,  # Property to animate
+			start,  # Start value
+			end,  # End value
+			time,  # Duration in seconds
+			Tween.TRANS_LINEAR,  # Transition type
+			Tween.EASE_IN_OUT  # Ease type
+		)
+		if prop == 'value': #connect to showing colored icon func
+			tween.connect("tween_all_completed",self,"_on_completed")
+		tween.start()
